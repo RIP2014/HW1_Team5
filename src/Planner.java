88885fe41867c2f2cff4d25f89505bf91c2f14dd
zsuffern0614 <shaunvons@lawn-128-61-58-124.lawn.gatech.edu>
2014-09-30 13:03:36 -0400

    random commit

--- src/Planner.java
+++ src/Planner.java
@@ -4,14 +4,36 @@ import java.util.PriorityQueue;
 public class Planner {
 	
 	PriorityQueue<Integer> stack;
+	int goal, soln;//change
+	int[] actionSet;//change
 	
 	public Planner(){
 		this.stack = new PriorityQueue<Integer>();
+		this.actionSet = new int[5];
+		this.goal = 5;//change
+		this.soln = 10;//change
 	}
 	
 	public void solve(){
+		this.stack.add(6);
+		//use bidirectional breadth first search?
+		while(!this.stack.isEmpty() || this.soln == this.goal){
+			int curr = this.stack.remove();
+			for(int a : actionSet){//change
+				int next = transistion(curr, a);//change
+				if(next == this.goal){
+					return;//chnage will have to get plan
+				}
+				int cost = aStar(next);//change
+				this.stack.add(next,cost);//change
+			}
+		}
 		
+		return;
+	}
+	
+	public int aStar(int next){
 		
-		
+		return 0;
 	}
 }
