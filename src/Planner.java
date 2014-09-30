eff59cee2833cd0322d748c836eb55e06004a04c
zsuffern0614 <shaunvons@lawn-128-61-58-124.lawn.gatech.edu>
2014-09-30 12:42:49 -0400

    Working on planner

--- src/Planner.java
+++ src/Planner.java
@@ -1,4 +1,17 @@
+import java.util.PriorityQueue;
+
 
 public class Planner {
-
+	
+	PriorityQueue<Integer> stack;
+	
+	public Planner(){
+		this.stack = new PriorityQueue<Integer>();
+	}
+	
+	public void solve(){
+		
+		
+		
+	}
 }
